2f96568c729a1f82873d752766fbdfc8a13bd311
Denis <Denis>
2020-06-16 13:02:43 +0200

    added the paged list

---
+++ Project.Common/System/PagedList/IPagedList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Project.Common.System
+{
+    public interface IPagedList<T> : IList<T>
+    {
+        int CurrentPage { get; }
+        int TotalPages { get; }
+        int PageSize { get; set; }
+        int TotalCount { get; set; }
+        bool HasPrevious { get; }
+        bool HasNext { get; }
+    }
+}
