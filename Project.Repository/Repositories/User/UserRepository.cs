097e194ce5f3f87b1f8140bcc730b4044d490aec
Denis <Denis>
2020-06-17 00:25:40 +0200

    implemented sorting and filtering methods

--- Project.Repository/Repositories/User/UserRepository.cs
+++ Project.Repository/Repositories/User/UserRepository.cs
@@ -1,10 +1,16 @@
-﻿using Project.Common.Application;
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Common.Application;
+using Project.Common.System;
 using Project.DAL;
 using Project.DAL.EntityModels;
 using Project.Repository.Common;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
 
 namespace Project.Repository
 {
@@ -14,6 +20,99 @@ namespace Project.Repository
         {
 
         }
+
+        public Task<IPagedList<UserEntity>> FindUserAsync(UserParameters parameters)
+        {
+            IQueryable<UserEntity> query = UnitOfWork.Context.Set<UserEntity>();
+
+            InitializeFilter(ref query, parameters);
+            InitializeSorting(ref query, parameters.OrderBy);
+
+            return base.FindAsync(parameters, query);
+        }
+
+
+        public void InitializeFilter(ref IQueryable<UserEntity> query, UserParameters parameters)
+        {
+
+            if (string.IsNullOrWhiteSpace(parameters.Name))
+            {
+                query.Where(x => EF.Functions.Like(x.Name, $"%{parameters.Name}%"));
+            }
+            if (parameters.DateCreated != null)
+            {
+                query.Where(x => x.DateCreated < parameters.DateCreated);
+            }
+
+            base.InitializeFilter(ref query, parameters);
+
+        }
+
+        public new void InitializeSorting(ref IQueryable<UserEntity> query, string orderByQueryString)
+        {
+
+            //if (string.IsNullOrWhiteSpace(orderByQueryString))
+            //{
+            //    query = query.OrderBy(entity => entity.Name);
+            //    return;
+            //}
+
+            base.InitializeSorting(ref query, orderByQueryString);
+        }
+
+        //public void  InitializeSorting(ref IQueryable<UserEntity> query, string orderByQueryString)
+        //{
+        //    if (!query.Any())
+        //    {
+        //        return;
+        //    }
+
+        //    if (string.IsNullOrWhiteSpace(orderByQueryString))
+        //    {
+        //        query = query.OrderBy(entity => entity.Name);
+        //        return;
+        //    }
+
+        //    var orderParams = orderByQueryString.Trim().Split(',');
+        //    var properyInfos = typeof(UserEntity).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        //    var orderQueryBuilder = new StringBuilder();
+
+        //    foreach (var param in orderParams)
+        //    {
+        //        if (string.IsNullOrWhiteSpace(param))
+        //        {
+        //            continue;
+        //        }
+
+        //        var propertyFromQueryName = param.Split(" ")[0];
+        //        var objectProperty = properyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+
+
+        //        if(objectProperty == null)
+        //        {
+        //            continue;
+        //        }
+
+        //        var sortingOrder = param.EndsWith(" desc") ? "descending" : "ascending";
+
+        //        orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {sortingOrder}, ");
+
+        //    }
+
+        //    var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+
+
+        //    if (string.IsNullOrWhiteSpace(orderQuery))
+        //    {
+        //        query = query.OrderBy(x => x.Name);
+        //        return;
+        //    }
+
+        //    query = query.OrderBy(orderQuery);
+
+        //}
+
+
 
     }
 }
--- Project.Repository/Repository.cs
+++ Project.Repository/Repository.cs
@@ -8,13 +8,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
 using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
 
 namespace Project.Repository
 {
     public class Repository<TEntity> : IRepository<TEntity> where TEntity : class, IBaseEntity
     {
-        private IUnitOfWork UnitOfWork { get; set; }
+        protected IUnitOfWork UnitOfWork { get; set; }
 
         public Repository(IUnitOfWork unitOfWork)
         {
@@ -64,14 +67,22 @@ namespace Project.Repository
             if (entity != null) UnitOfWork.Context.Set<TEntity>().Remove(entity);
         }
 
-        public virtual async Task<PagedList<TEntity>> FindAsyncNoTracking(Parameters parameters)
+        public virtual async Task<IPagedList<TEntity>> FindAsyncNoTracking<TParameters>(TParameters parameters) where TParameters : IParameters
         {
             return await PagedList<TEntity>.ToPagedListAsync(UnitOfWork.Context.Set<TEntity>().AsNoTracking(), parameters.PageNumber, parameters.PageSize);
         }
 
-        public virtual async Task<IEnumerable<TEntity>> FindAsync(Parameters parameters)
+        public virtual async Task<IPagedList<TEntity>> FindAsync<TParameters>(TParameters parameters, IQueryable<TEntity> source = null) where TParameters : IParameters
         {
-            return await UnitOfWork.Context.Set<TEntity>().Skip((parameters.PageNumber - 1) * parameters.PageSize).Take(parameters.PageSize).ToListAsync();
+            if(source == null)
+            {
+                return await PagedList<TEntity>.ToPagedListAsync(UnitOfWork.Context.Set<TEntity>(), parameters.PageNumber, parameters.PageSize);
+            }
+            else
+            {
+                return await PagedList<TEntity>.ToPagedListAsync(source, parameters.PageNumber, parameters.PageSize);
+            }
+
         }
 
         public virtual async Task<TEntity> GetAsyncNoTracking(Guid id)
@@ -84,10 +95,10 @@ namespace Project.Repository
             return await UnitOfWork.Context.Set<TEntity>().SingleOrDefaultAsync(x => x.Id == id);
         }
 
-        public virtual IAsyncEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
-        {
-            return UnitOfWork.Context.Set<TEntity>().Where(predicate).AsAsyncEnumerable();
-        }
+        //public virtual IAsyncEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        //{
+        //    return UnitOfWork.Context.Set<TEntity>().Where(predicate).AsAsyncEnumerable();
+        //}
 
         public virtual TEntity Update(TEntity entity)
         {
@@ -96,5 +107,74 @@ namespace Project.Repository
             UnitOfWork.Context.Update(entity);
             return entity;
         }
+
+        public void InitializeFilter<TParameters>(ref IQueryable<TEntity> query, TParameters parameters) where TParameters : IParameters
+        {
+            if(parameters.IsActive)
+            {
+                query.Where(entity => entity.IsActive == true);
+            }
+        }
+
+        /// <summary>
+        /// extract sort parameters from sort query
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="orderByQueryString"></param>
+        /// <param name="defaultSort">If default sort is true sorting will be done by id, use this when you don't implement default sort</param>
+        public virtual void InitializeSorting(ref IQueryable<TEntity> query, string orderByQueryString)
+        {
+            if (!query.Any())
+            {
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+            {
+                query = query.OrderBy(entity => entity.Id);
+                return;
+            }
+
+            var orderParams = orderByQueryString.Trim().Split(',');
+            var properyInfos = typeof(TEntity).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var orderQueryBuilder = new StringBuilder();
+
+            foreach (var param in orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                {
+                    continue;
+                }
+
+                var propertyFromQueryName = param.Split(" ")[0];
+                var objectProperty = properyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+
+
+                if (objectProperty == null)
+                {
+                    continue;
+                }
+
+                var sortingOrder = param.EndsWith(" desc") ? "descending" : "ascending";
+
+                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {sortingOrder}, ");
+
+            }
+
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+
+
+            if (string.IsNullOrWhiteSpace(orderQuery))
+            {
+                query = query.OrderBy(x => x.Id);
+                return;
+            }
+
+            query = query.OrderBy(orderQuery);
+
+        }
+
+
+
     }
 }
