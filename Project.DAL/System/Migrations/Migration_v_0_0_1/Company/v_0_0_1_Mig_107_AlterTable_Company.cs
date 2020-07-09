5c84ecc270b5fc44d550f5dcd0efa0f629b70944
Denis <Denis>
2020-07-09 18:53:51 +0200

    created the cs and sql to alter company table for unique constraint on the name column, created the new context for the change

---
+++ Project.DAL/System/Migrations/Migration_v_0_0_1/Company/v_0_0_1_Mig_107_AlterTable_Company.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+using System;
+
+namespace Project.DAL.System
+{
+    /// <summary>
+    /// Alter company table
+    /// </summary>
+    [Migration(107, "Alter company table: add unique constaint to the name column")]
+    public class v_0_0_1_Mig_107_AlterTable_Company : Migration
+    {
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Up()
+        {
+            Execute.EmbeddedScript("v_0_0_1_Mig_107_AlterTable_Company.sql");
+        }
+    }
+}
---
+++ Project.DAL/System/Migrations/Migration_v_0_0_1/Company/v_0_0_1_Mig_107_AlterTable_Company.sql
@@ -0,0 +1,11 @@
+﻿DO $$
+BEGIN
+	IF (NOT EXISTS(SELECT 1 FROM information_schema."constraint_column_usage" info WHERE info.table_name = 'Company' and info.column_name= 'Name')) THEN
+			ALTER TABLE "public"."Company" ADD CONSTRAINT UC_Company_Name UNIQUE ("Name");
+
+			RAISE NOTICE 'Added UNIQUE constraint to the name column';
+		ELSE
+			RAISE NOTICE 'The UNIQUE constraint already exists on the Name column';
+	END IF;
+
+END $$
