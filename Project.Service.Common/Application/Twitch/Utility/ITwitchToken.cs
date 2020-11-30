3d03029e7f31641b117c9cc834b6794e00c9dfde
Denis <Denis>
2020-11-30 20:25:31 +0100

    twitch token

---
+++ Project.Service.Common/Application/Twitch/Utility/ITwitchToken.cs
@@ -0,0 +1,15 @@
+﻿namespace Project.Service.Common.Application.Twitch.Utility
+{
+    public interface ITwitchToken
+    {
+        string access_token { get; set; }
+
+        string refresh_token { get; set; }
+
+        int expires_in { get; set; }
+
+        string[] scope { get; set; }
+
+        string token_type { get; set; }
+    }
+}
